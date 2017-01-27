269acb25a06524a547b933fbe2291a943b1c4660
carvalhodj <danieljosecarvalho@gmail.com>
2017-01-27 13:52:21 -0300

    Layout da tela de busca do usuário comum

---
+++ app/src/main/java/com/tonight/tonight/GUI/SearchEventActivity.java
@@ -0,0 +1,15 @@
+package com.tonight.tonight.GUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tonight.tonight.R;
+
+public class SearchEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_event);
+    }
+}
